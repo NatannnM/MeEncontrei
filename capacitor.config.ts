3660d4f3fa578a9d52ddc4d82391f8b0a52bfac3
NatannnM <nata_martins@outlook.com>
2025-05-28 23:32:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MeEncontrei',
+  webDir: 'www'
+};
+
+export default config;
